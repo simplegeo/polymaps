a6fb651b8ff069b4d6acc84cd8bea402eaf96908
Mike Bostock <mbostock+gitorious@gmail.com>
2010-08-14 22:49:23 -0700

    Fix broken namespace in kml example.

--- examples/kml/kml.js
+++ examples/kml/kml.js
@@ -1,59 +1,63 @@
-po.kml = function() {
-  var kml = po.geoJson(fetch);
+(function() {
+  var po = org.polymaps;
 
-  function fetch(url, update) {
-    po.queue.xml(url, function(xml) {
-      update(geoJson(xml));
-    });
-  }
+  po.kml = function() {
+    var kml = po.geoJson(fetch);
 
-  var types = {
+    function fetch(url, update) {
+      po.queue.xml(url, function(xml) {
+        update(geoJson(xml));
+      });
+    }
 
-    Point: function(e) {
-      return {
-        type: "Point",
-        coordinates: e.getElementsByTagName("coordinates")[0]
+    var types = {
+
+      Point: function(e) {
+        return {
+          type: "Point",
+          coordinates: e.getElementsByTagName("coordinates")[0]
             .textContent
             .split(",")
             .map(Number)
-      };
-    },
+        };
+      },
 
-    LineString: function(e) {
-      return {
-        type: "LineString",
-        coordinates: e.getElementsByTagName("coordinates")[0]
+      LineString: function(e) {
+        return {
+          type: "LineString",
+          coordinates: e.getElementsByTagName("coordinates")[0]
             .textContent
             .trim()
             .split(/\s+/)
             .map(function(a) { return a.split(",").slice(0, 2).map(Number); })
-      };
+        };
+      }
+
+    };
+
+    function geometry(e) {
+      return e && e.tagName in types && types[e.tagName](e);
     }
 
-  };
-
-  function geometry(e) {
-    return e && e.tagName in types && types[e.tagName](e);
-  }
-
-  function geoJson(xml) {
-    var features = [],
-        placemarks = xml.getElementsByTagName("Placemark");
-    for (var i = 0; i < placemarks.length; i++) {
-      var e = placemarks[i],
-          f = {id: e.getAttribute("id"), properties: {}};
-      for (var c = e.firstChild; c; c = c.nextSibling) {
-        switch (c.tagName) {
+    function geoJson(xml) {
+      var features = [],
+      placemarks = xml.getElementsByTagName("Placemark");
+      for (var i = 0; i < placemarks.length; i++) {
+        var e = placemarks[i],
+        f = {id: e.getAttribute("id"), properties: {}};
+        for (var c = e.firstChild; c; c = c.nextSibling) {
+          switch (c.tagName) {
           case "name": f.properties.name = c.textContent; continue;
           case "description": f.properties.description = c.textContent; continue;
+          }
+          var g = geometry(c);
+          if (g) f.geometry = g;
         }
-        var g = geometry(c);
-        if (g) f.geometry = g;
+        if (f.geometry) features.push(f);
       }
-      if (f.geometry) features.push(f);
+      return {features: features};
     }
-    return {features: features};
-  }
 
-  return kml;
-};
+    return kml;
+  };
+})();
