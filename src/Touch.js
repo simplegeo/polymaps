cb160c516cd14528f76c29c6faffb6bdad078e6b
Nathan Vander Wilt <nate@andyet.net>
2011-03-18 22:22:29 +0100

    basic port of the touch handler code from https://github.com/andyet/OpenLayers-Multitouch

--- src/Touch.js
+++ src/Touch.js
@@ -1,94 +1,74 @@
 po.touch = function() {
   var touch = {},
       map,
-      prevPoints = {},
-      startDist = null,
-      startCenter = null,
-      scaleFactor = null,
-      center = null;
+      last;
 
   touch.map = function(x) {
     if (!arguments.length) return map;
     map = x;
     // TODO remove from old map container?
     // TODO update if map container changes?
-    map.container().addEventListener("touchstart", touchstart, false);
+    var container = map.container();
+    container.addEventListener("touchstart", touchstart, false);
+    container.addEventListener("touchmove", touchmove, false);
+    container.addEventListener("touchend", touchend, false);
+    container.addEventListener("touchcancel", touchend, false);
     return touch;
   }
-
-  function client(t) {
-    return { x: t.clientX, y: t.clientY };
+  
+  function updateMap(last, now) {
+    var delta = {
+      x: now.clientX - last.clientX,
+      y: now.clientY - last.clientY,
+      scale: now.scale - last.scale
+    };
+		if (delta.x || delta.y || delta.scale) {
+      var oldPoint = map.mouse(last),
+          location = map.pointLocation(oldPoint);
+      map.zoomBy(delta.scale, map.mouse(now), location);
+		}
   }
-
-  function distance(p1, p2) {
-    var dx = p2.x - p1.x,
-        dy = p2.y - p1.y;
-    return Math.sqrt(dx*dx + dy*dy);
-  }
-    
-  function interpolate(p1, p2, t) {
-    var px = p1.x + (p2.x - p1.x) * t,
-        py = p1.y + (p2.y - p1.y) * t;
-    return { x: px, y: py };
-  }
-
+  
   function touchstart(e) {
-    for (var i = 0; i < e.touches.length; i++) {
-      if (e.touches[i].identifier in prevPoints) continue;
-      prevPoints[e.touches[i].identifier] = client(e.touches[i]);
-    }
-    if (e.touches.length == 2) {
-      var p0 = prevPoints[e.touches[0].identifier];
-          p1 = prevPoints[e.touches[1].identifier];
-      startDist = distance(p0, p1);
-      startCenter = interpolate(p0, p1, 0.5);                    
-    }
+		if (last) {
+			// if new finger being added, finish current move sequence first
+			touchend();
+		}
     e.preventDefault();
-    return false;
-  }
-  function touchmove(e) {
-    if (e.touches.length == 1) {
-      var prevTouch = prevPoints[e.touches[0].identifier],
-          thisTouch = client(e.touches[0]);
-      map.panBy({ x: thisTouch.x - prevTouch.x, y: thisTouch.y - prevTouch.y });
-      prevPoints[e.touches[0].identifier] = thisTouch;
-    }
-    if (e.touches.length == 2) {
-      var p0 = client(e.touches[0]),
-          p1 = client(e.touches[1]);
-          dist = distance(p0,p1);
-      scaleFactor = dist / startDist;
-      center = interpolate(p0, p1, 0.5);
-      map.container().style.webkitTransformOrigin = startCenter.x + 'px ' + startCenter.y + 'px'; 
-      map.container().style.webkitTransform = 'translate3d('+ (center.x-startCenter.x) +'px,' 
-                                                      + (center.y-startCenter.y) +'px,0px) '
-                                                      + 'scale3d('+ scaleFactor +','+ scaleFactor +',1) ';
-      prevPoints[e.touches[0].identifier] = p0;
-      prevPoints[e.touches[1].identifier] = p1;
-    }
+    
+		last = touchesCenter(e);
+	}
+
+	function touchmove(e) {
+		if (!last) {
+			// if finger was lifted, start new move sequence instead
+			return touchstart(e);
+		}
     e.preventDefault();
-    return false;
-  }
-  function touchend(e) {
-    if (e.touches.length < 2 && scaleFactor) {
-      map.container().style.webkitTransformOrigin = '';
-      map.container().style.webkitTransform = '';
-      map.panBy({ x: -startCenter.x, y: -startCenter.y });
-      map.zoomBy(Math.log(scaleFactor)/Math.LN2, { x: 0, y: 0 });
-      map.panBy({ x: center.x-startCenter.x, y: center.y-startCenter.y });
-      map.panBy({ x: startCenter.x, y: startCenter.y });
-      scaleFactor = undefined;
-      center = undefined;
-    }
-    for (var i = 0; i < e.changedTouches.length; i++) {
-      if (e.changedTouches[i].identifier in prevPoints) {
-        delete prevPoints[e.changedTouches[i].identifier];
-      }
-    }
+    
+		var now = touchesCenter(e);
+    updateMap(last, now);
+    last = now;
   }
 
-  window.addEventListener("touchmove", touchmove, false);
-  window.addEventListener("touchend", touchend, false);
+	function touchend(e) {
+		last = null;
+	}
 
+	function touchesCenter(e) {
+		var touches = e.touches;
+		var sumX = 0, sumY = 0;
+		for (var i = 0; i < touches.length; ++i) {
+			var touch = touches[i];
+			sumX += touch.clientX;
+			sumY += touch.clientY;
+		}
+		return {
+			clientX: sumX / touches.length,
+      clientY: sumY / touches.length,
+      scale: e.scale
+    };
+	}
+  
   return touch;
 };
