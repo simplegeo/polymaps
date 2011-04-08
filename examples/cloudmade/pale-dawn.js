cac36b1fe9cc5a7b4d81940ee4ab54fcb8aff57a
Michael Bostock <mbostock@gmail.com>
2011-04-07 22:17:27 -0700

    Enable repeat.

--- examples/cloudmade/pale-dawn.js
+++ examples/cloudmade/pale-dawn.js
@@ -9,7 +9,6 @@ map.add(po.image()
     .url(po.url("http://{S}tile.cloudmade.com"
     + "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
     + "/998/256/{Z}/{X}/{Y}.png")
-    .repeat(false)
     .hosts(["a.", "b.", "c.", ""])));
 
 map.add(po.compass()
